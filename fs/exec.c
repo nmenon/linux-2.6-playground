9ba6baa943a02e1d11fc6cea1bc72c450995e2b3
Oleg Nesterov <oleg@redhat.com>
2013-06-19 10:08:23 +1000

    fs/exec.c: do_execve_common(): use current_user()
    Trivial cleanup.  do_execve_common() can use current_user() and avoid the
    unnecessary "struct cred *cred" var.
    
    Signed-off-by: Oleg Nesterov <oleg@redhat.com>
    Cc: Vasiliy Kulikov <segoon@openwall.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- fs/exec.c
+++ fs/exec.c
@@ -1462,7 +1462,6 @@ static int do_execve_common(const char *filename,
 	struct files_struct *displaced;
 	bool clear_in_exec;
 	int retval;
-	const struct cred *cred = current_cred();
 
 	/*
 	 * We move the actual failure in case of RLIMIT_NPROC excess from
@@ -1471,7 +1470,7 @@ static int do_execve_common(const char *filename,
 	 * whether NPROC limit is still exceeded.
 	 */
 	if ((current->flags & PF_NPROC_EXCEEDED) &&
-	    atomic_read(&cred->user->processes) > rlimit(RLIMIT_NPROC)) {
+	    atomic_read(&current_user()->processes) > rlimit(RLIMIT_NPROC)) {
 		retval = -EAGAIN;
 		goto out_ret;
 	}
