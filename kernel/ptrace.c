76f30de6f32a2de846d2ee7dc03353bebccfdc9a
Andrew Morton <akpm@linux-foundation.org>
2013-06-19 10:08:17 +1000

    ptrace-add-ability-to-get-set-signal-blocked-mask-fix
    be consistent with brace layout
    
    Cc: Andrey Vagin <avagin@openvz.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- kernel/ptrace.c
+++ kernel/ptrace.c
@@ -856,8 +856,7 @@ int ptrace_request(struct task_struct *child, long request,
 
 		break;
 
-	case PTRACE_SETSIGMASK:
-	{
+	case PTRACE_SETSIGMASK: {
 		sigset_t new_set;
 
 		if (addr != sizeof(sigset_t)) {
@@ -989,8 +988,7 @@ int ptrace_request(struct task_struct *child, long request,
 
 #ifdef CONFIG_HAVE_ARCH_TRACEHOOK
 	case PTRACE_GETREGSET:
-	case PTRACE_SETREGSET:
-	{
+	case PTRACE_SETREGSET: {
 		struct iovec kiov;
 		struct iovec __user *uiov = datavp;
 
