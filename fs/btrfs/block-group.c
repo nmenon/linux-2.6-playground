ff51bf02d1071a01f21db14bb239e1a8db8d09b3
Marcos Paulo de Souza <mpdesouza@suse.com>
2020-08-21 11:54:44 -0300

    btrfs: block-group: fix free-space bitmap threshold
    [BUG]
    After commit 9afc66498a0b ("btrfs: block-group: refactor how we read one
    block group item"), cache->length is being assigned after calling
    btrfs_create_block_group_cache. This causes a problem since
    set_free_space_tree_thresholds calculates the free-space threshold to
    decide if the free-space tree should convert from extents to bitmaps.
    
    The current code calls set_free_space_tree_thresholds with cache->length
    being 0, which then makes cache->bitmap_high_thresh zero. This implies
    the system will always use bitmap instead of extents, which is not
    desired if the block group is not fragmented.
    
    This behavior can be seen by a test that expects to repair systems
    with FREE_SPACE_EXTENT and FREE_SPACE_BITMAP, but the current code only
    created FREE_SPACE_BITMAP.
    
    [FIX]
    Call set_free_space_tree_thresholds after setting cache->length. There
    is now a WARN_ON in set_free_space_tree_thresholds to help preventing
    the same mistake to happen again in the future.
    
    Link: https://github.com/kdave/btrfs-progs/issues/251
    Fixes: 9afc66498a0b ("btrfs: block-group: refactor how we read one block group item")
    CC: stable@vger.kernel.org # 5.8+
    Reviewed-by: Qu Wenruo <wqu@suse.com>
    Reviewed-by: Filipe Manana <fdmanana@suse.com>
    Signed-off-by: Marcos Paulo de Souza <mpdesouza@suse.com>
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/block-group.c
+++ fs/btrfs/block-group.c
@@ -1798,7 +1798,6 @@ static struct btrfs_block_group *btrfs_create_block_group_cache(
 
 	cache->fs_info = fs_info;
 	cache->full_stripe_len = btrfs_full_stripe_len(fs_info, start);
-	set_free_space_tree_thresholds(cache);
 
 	cache->discard_index = BTRFS_DISCARD_INDEX_UNUSED;
 
@@ -1907,6 +1906,8 @@ static int read_one_block_group(struct btrfs_fs_info *info,
 		return -ENOMEM;
 
 	read_block_group_item(cache, path, key);
+
+	set_free_space_tree_thresholds(cache);
 
 	if (need_clear) {
 		/*
@@ -2128,6 +2129,7 @@ int btrfs_make_block_group(struct btrfs_trans_handle *trans, u64 bytes_used,
 		return -ENOMEM;
 
 	cache->length = size;
+	set_free_space_tree_thresholds(cache);
 	cache->used = bytes_used;
 	cache->flags = type;
 	cache->last_byte_to_unpin = (u64)-1;
